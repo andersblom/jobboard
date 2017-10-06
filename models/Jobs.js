5724f31c1b0b25a560dc609536d37cf2c181a6a1
Anders Blom <anders@andersblom.dk>
2017-10-06 12:04:10 -0400

    node schedule note/todo

--- models/Jobs.js
+++ models/Jobs.js
@@ -34,6 +34,8 @@ jobPostingSchema.pre("save", function(next) {
     // String replacement: line breaks getting swapped out to <br />'s 
      let newBody = (this.body + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + "<br />" + '$2');
     
+     // TODO: Look into creating a cronjob/node-schedule that sets the "active" to false whenever expirty date is reached: https://github.com/node-schedule/node-schedule
+
      // Sending the new string with <br />'s back 
      this.body = newBody;
      next();
